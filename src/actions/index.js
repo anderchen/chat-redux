d2ba5607d3d951098ebbbc9859f8f9e83c9f2c45
Anderson Chen <ander.chen00@gmail.com>
2019-03-13 17:08:25 -0300

    Added an initial state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // TODO: add and export your own actions
+const initialState = {
+  messages: [],
+  channels: [general, react, paris],
+  selectedChannel: 'general',
+  currentUser: prompt("What is your username?") || `anonymous` 
+
+}
