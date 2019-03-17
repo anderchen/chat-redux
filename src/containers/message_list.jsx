c681ea13d25362c4174652ae28a541bca1bd154c
Anderson Chen <ander.chen00@gmail.com>
2019-03-17 17:54:31 -0300

    Changed the code in the reducers

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -2,37 +2,59 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import { fetchMessages } from '../actions';
 import Message from '../components/message';
+import MessageForm from '../containers/message_form';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class MessageList extends Component {
+  componentWillMount() {
+    this.fetchMessages();
+  }
 
-  render() {
+  componentDidMount() {
+    this.refresher = setInterval(this.fetchMessages, 5000);
+  }
+
+  componentDidUpdate() {
+    this.list.scrollTop = this.list.scrollHeight;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.refresher);
+  }
+
+  fetchMessages = () => {
+    this.props.fetchMessages(this.props.selectedChannel);
+  }
+
+  render () {
     return (
-      <div className="message-list">
-        {this.props.messages.map((message) => {
-          return <Message  key={message.created_at} message={message} />;
-        })}
+      <div className="channel-container">
+        <div className="channel-title">
+          <span>Channel #{this.props.selectedChannel}</span>
+        </div>
+        <div className="channel-content" ref={(list) => { this.list = list; }}>
+          {
+            this.props.messages.map((message) => {
+              return <Message key={message.id} message={message} />;
+            })
+          }
+        </div>
+        <MessageForm />
       </div>
     );
   }
 }
 
-function mapStateToProps(){
+function mapStateToProps(state) {
   return {
-    messages: [
-      {
-        "author":"anonymous92",
-        "content":"Hello world!",
-        "created_at":"2017-09-26T23:03:16.365Z"
-      },
-      {
-        "author":"anonymous77",
-        "content":"My name is anonymous77",
-        "created_at":"2017-09-26T23:03:21.194Z"
-      }
-    ]
-  }
+    messages: state.messages,
+    selectedChannel: state.selectedChannel
+  };
 }
 
-export default connect(mapStateToProps) (MessageList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchMessages }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
